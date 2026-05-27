0d5e2c8c31a069e3b1f11c0ba6f8d385236f6ec2
ItsMeDrip <darshsomani4@gmail.com>
2026-05-27 14:49:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
+const mineflayer = require('mineflayer')
 
+function createBot() {
+  const bot = mineflayer.createBot({
+    host: 'PUT_YOUR_IP_HERE',
+    port: 25565,
+    username: 'DrippyBot',
+    version: '1.8.9',
+    auth: 'offline'
+  })
+
+  bot.on('spawn', () => {
+    console.log('Drippy Bot is online! 🔥')
+
+    // Anti AFK - jumps every 30 seconds
+    setInterval(() => {
+      bot.setControlState('jump', true)
+      setTimeout(() => {
+        bot.setControlState('jump', false)
+      }, 500)
+    }, 30000)
+  })
+
+  bot.on('kicked', (reason) => {
+    console.log('Bot got kicked:', reason)
+    setTimeout(createBot, 5000)
+  })
+
+  bot.on('error', (err) => {
+    console.log('Error:', err)
+    setTimeout(createBot, 5000)
+  })
+
+  bot.on('end', () => {
+    console.log('Bot disconnected, reconnecting...')
+    setTimeout(createBot, 5000)
+  })
+}
+
+createBot()
